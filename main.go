8d35d9350d6acf05bbcab5da60ef34c5bd40f56f
Bryce Pittman <pittsoccer101@gmail.com>
2026-02-12 10:15:57 -0600

    Refactor handler to use HTML for response
    Updated the handler function to output HTML format and removed commented code.

--- main.go
+++ main.go
@@ -7,16 +7,15 @@ import (
 )
 
 func handler(response http.ResponseWriter, request *http.Request) {
-	//fmt.Fprintf(response, "Hello %s!", request.URL.Path[1:])
-	fmt.Fprintf(response, "Hello Bryce!")
-	fmt.Fprintf(response, "\n")
+	// output user
+	fmt.Fprintf(response, "<h1>Hello Bryce!</h1>")
+
+	// output time
+	fmt.Fprintf(response, "<h3> The time is: ")
 	fmt.Fprintf(response, time.Now().Format(time.RFC1123))
 }
 
 func main() {
-	//s := "gopher"
-	//fmt.Printf("Hello and welcome, %s!\n", s)
-
 	http.HandleFunc("/", handler)
 	http.ListenAndServe("", nil)
 }
